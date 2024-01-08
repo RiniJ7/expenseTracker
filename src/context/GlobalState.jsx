ff8f0a37c88df3c7d4e2afd73a79c539a926d562
rinij <rinimani7@gmail.com>
2024-01-07 20:07:33 -0700

    created dummy transactions

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { createContext, useReducer }from 'react';
+
+
+//initial state
+const initialState= {
+    transactions: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Cammera', amount: 150 }
+    ]
+}
+
+// Create context
+
 
 const GlobalProvider = () => {
   return (
