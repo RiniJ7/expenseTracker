887e24d094327620a8d14e15268cb2154317e0a5
rinij <rinimani7@gmail.com>
2024-01-11 14:59:11 -0700

    added routes at App level

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,23 @@ import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 
 // import  GlobalProvider from './context/GlobalState';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/auth/index";
+import { ExpenseTracker } from "./pages/expense-tracker/index";
 import './App.css';
 
 function App() {
   return (
     <>
+    <div className="App">
+<Router>
+  <Routes>
+    <Route path="/"  element={<Auth />} />
+    <Route path="/expense-tracker"  element={<ExpenseTracker />} />
+
+  </Routes>
+</Router>
+    </div>
     {/* <GlobalProvider> */}
       <Header />
       <div className="container">
