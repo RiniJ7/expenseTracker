fb267aa1809f2e2c2ad2d5bb17bbfbfcc00e9507
rinij <rinimani7@gmail.com>
2024-01-11 17:22:04 -0700

    initialize firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey : `${process.env.REACT_APP_EXPENSE_TRACKER_API_KEY}`,
+  authDomain : `${process.env.REACT_APP_EXPENSE_TRACKER_AUTH_DOMAIN}`,
+  projectId : `${process.env.REACT_APP_EXPENSE_TRACKER_PROJECT_ID}`,
+  storageBucket : `${process.env.REACT_APP_EXPENSE_TRACKER_STORAGE_BUCKET}`,
+  messagingSenderId : `${process.env.REACT_APP_EXPENSE_TRACKER_MESSAGING_SENDER_ID}`,
+  appId : `${process.env.REACT_APP_EXPENSE_TRACKER_APP_ID}`,
+  measurementId : `${process.env.REACT_APP_EXPENSE_TRACKER_MEASUREMENT_ID}`
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth =getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+//commands
+// firebase login
+// firebase init
+// firebase deploy
